048b081018698af26432f2e67bc9d633b2284ed2
David Sooter <david.sooter@ext.sys.aok.de>
2020-02-12 13:36:15 +0100

    preps before publish

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,3 @@ import createClient from './ProngClientFactory';
   window.createClient = createClient;
 })(window);
 
-/*const prongClient = createClient({
-  url: 'https://iot.cpars.de/prong/',
-  credentials: {
-    username: 'root',
-    client_id: 'b3f3b982-16ab-11ea-83b0-000c29effd1c',
-    client_secret: 'saqw21!@',
-    password: 'Sommer1!',
-  },
-});
-
-prongClient.get('Equipment', 's4demo', {id: '217100001'}).then(
-  function(result) {
-    console.log(result);
-  },
-  function(error) {
-    console.log(error);
-  },
-);
-*/
