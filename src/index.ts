99a649f33a21c77c6e25373a8e651a4a81831fc6
David Sooter <david.sooter@ext.sys.aok.de>
2020-02-13 10:16:50 +0100

    set main

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-export * from './lib/ProngClientFactory';
+import { CodeCredentials, ProngClientType, Clients } from './lib/types';
+import { ProngConfigClient, ProngResourceClient, ProngProxyClient } from './lib/clients';
+
 export * from './lib/types';
+
+/**
+ * Factory Method for creating a prong client.
+ * The default client is the Resource Client since that is what is most often used
+ * you can control which client is created by passing the type as the second
+ * argument to the factory.
+ * @param config
+ * @param type
+ */
+export function createClient(config: {url: string; credentials: Required<CodeCredentials>}, type: ProngClientType = ProngClientType.RESOURCE): Clients {
+    switch (type) {
+      case ProngClientType.CONFIG:
+        return new ProngConfigClient(config);
+      case ProngClientType.RESOURCE:
+        return new ProngResourceClient(config);
+      case ProngClientType.PROXY:
+        return new ProngProxyClient(config);
+    }
+  }
+  
